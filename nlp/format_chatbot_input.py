54b21f6730d1cbd9d8a677ca67193d826cc52406
NikhilJoshi28 <nikhiljoshi1nikhil@gmail.com>
2017-03-21 14:41:15 +0530

    Update format_chatbot_input.py
    Made more descriptive

--- nlp/format_chatbot_input.py
+++ nlp/format_chatbot_input.py
@@ -10,6 +10,13 @@ def format_(inp_string):
 			
 	#removing common words(stopwords)
 	filtered_string = [word for word in words if not word in stop_words]
+	"""
+	#one liner mensioned above
+	filtered_string=[]
+	for w in words:
+	if w not in stop_words:
+		filtered_string.append(w)
+	"""
 	
 	"""
 	more magic on filtered_string in list format
